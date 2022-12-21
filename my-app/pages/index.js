513143508385a030837c290c5f58e9edfa87c97a
taylorferran <tailzpk@hotmail.co.uk>
2022-12-21 17:09:37 +0800

    Change font+ add animations

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,21 +1,43 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import { motion } from "framer-motion";
+
 
 export default function Home() {
+
+  const [movePic, setMovePic] = useState(false);
+
   return (
     <div>
       <Head>
         <title>taylor ferran</title>
         <meta name="description" content="it's me" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@300&display=swap" rel="stylesheet" />
+        </Head>
 
       <main className={styles.main}>
+
+      <motion.div
+        animate={{
+                y: movePic ? 230 : 0,
+                scale: movePic ? 2.5 : 1,
+        }}
+        onClick={() => {
+          setMovePic(!movePic);
+        }}
+      >
         <img src="/taylorphoto.jpeg" width="200" height="266.67"></img>
-        <h1 className={styles.title}>
-           taylor ferran
-        </h1>
+      </motion.div>
+      <motion.div className={styles.main}
+        initial={{ opacity: 0, scale: 0.05 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 0.7 }}
+      
+      >
+      <h1>taylor ferran</h1>
 
         <b>about</b>
           <p>I'm from Belfast, Northern Ireland but currently I'm traveling around South East Asia.</p>
@@ -35,6 +57,8 @@ export default function Home() {
         <a href="https://www.linkedin.com/in/taylorferran/" target="_blank">LinkedIn</a>
         <a href="https://www.youtube.com/channel/UCTUKeaO8igoZ_1jddxfP1wA" target="_blank">Youtube</a>
         <a href="https://medium.com/@taylor_web3" target="_blank">Medium</a>
+        </motion.div >
+
       </main>
 
     </div>
