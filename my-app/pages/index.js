4f5938827dd4f97552e2a60d3a54c981cb8d4f46
taylorferran <tailzpk@hotmail.co.uk>
2023-08-21 15:01:21 +0100

    Add links

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -56,6 +56,8 @@ export default function Home() {
         <a href="https://twitter.com/taylor_web3" target="_blank">Twitter</a>
         <a href="https://github.com/taylorferran" target="_blank">Github</a>
         <a href="https://www.linkedin.com/in/taylorferran/" target="_blank">LinkedIn</a>
+        <a href="https://steamcommunity.com/id/taylorferran" target="_blank">Steam</a>
+        <a href="https://t.me/taylorferran" target="_blank">Telegram</a>
         <a href="https://medium.com/@taylor_web3" target="_blank">Medium</a>
         </motion.div >
 
